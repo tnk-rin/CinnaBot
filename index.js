55d479d493e751fd1981d973becfe8c9ad233602
Kazumik0108 <61595586+Kazumik0108@users.noreply.github.com>
2021-01-19 00:03:02 -0500

    new message.js module
    Functions for the emote command are now contained in the message.js file, which is required by index.js
    
    Changes: command no longer prompts the user for an input, since it can make chat messy, especially with large number of uses; if multiple name matches are found, it will prioritize the emote in the current server and then the first match.
    
    Potential issues: if the bot is in a large number of servers with overlapping emotes, the wrong emote may be prioritized; if a static and animated emote have the same name, there is no way to manually choose between them.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const { config } = require('dotenv');
 const { CommandoClient } = require('discord.js-commando');
 const path = require('path');
+const {getMessageEmotes, replaceMessageEmotes} = require('./events/message.js')
 config({path: `${__dirname}/process.env`});
 
 // Commando client
@@ -43,8 +44,8 @@ client.on('error', console.error);
 // client read messages
 client.on('message', message => {
 
+    // attempt to replace a message with a webhook
     const emotePrefix = '-';
-
     if (message.content.startsWith(emotePrefix)) {
         getMessageEmotes(message)
             .then(content => {
@@ -55,125 +56,11 @@ client.on('message', message => {
             });
     }
 
-    async function getMessageEmotes(message) {
-        // return early if author is bot
-        if (message.author.bot) return message.content.slice(1);
-        
-        // return early if length of split message contents is less than 3, since a pair of backticks makes it length >= 3
-        let content = message.content.toLowerCase().slice(1).split(/\`/);
-        if (content.length < 3) return message.content.slice(1);
+    
 
-        // get the emotes from each guild
-        let emoteCache = [];
-        let guilds = await message.client.guilds.cache;
-        guilds.forEach(guild => {
-            emoteCache.push(guild.emojis.cache)
-        });
+    
 
-        // try to match the emote name to every emote in each guild
-        const alphanum = /^[a-z0-9\_]+$/i;
-
-        content = await Promise.all(content.map(async substring => {
-            // return backticks for the empty substrings
-            if (substring === '') return '\`';
-
-            // return substring if not alphanumeric
-            if (substring.match(alphanum) === null) return substring;
-
-            let emotePicker = [];
-            for (let emotes of emoteCache) {
-                emotes.forEach(emote => {
-                    if (substring === emote.name.toLowerCase()) emotePicker.push(emote);
-                });
-            }
-
-            // return the original substring if no matches were found
-            if (emotePicker.length === 0) return substring;
-            
-            // return the first emote as a string if one match was found
-            if (emotePicker.length === 1) {
-                let emote = emotePicker.map(emote => {
-                    return (emote.animated) ? `<a:${emote.name}:${emote.id}>` : `<:${emote.name}:${emote.id}>`;
-                });
-                return emote.join('');
-            };
-
-            // return the first emote as a string if multiple matches were found (TEMPORARY)
-            if (emotePicker.length > 1) {
-                emote = await selectMessageEmote(message, emotePicker);
-                return emote;
-            };
-        })).then((results) => {
-            return results.join('');
-        })
-        
-        return content;
-
-    }
-
-    async function replaceMessageEmotes(message, content) {
-        message.delete();
-
-        const webhooks = await message.channel.fetchWebhooks();
-        const webhook = webhooks.first();
-
-        let member = message.guild.member(message.author);
-        let nickname = member ? member.displayName : null;
-        let avatar = message.author.displayAvatarURL();
-
-        if (typeof(webhook) === 'undefined') {                
-            // no webhook exists in this channel, so create one
-            message.channel.createWebhook('CinnaBot')
-                .then(webhook => {
-                    webhook.send(content, {
-                        username: nickname,
-                        avatarURL: avatar,
-                    });
-                });
-        } else {
-            // send the content through the existing channel webhook
-            webhook.send(content, {
-                username: nickname,
-                avatarURL: avatar,
-            });
-        }
-    }
-
-    async function selectMessageEmote(message, emoteDuplicate) {
-        let emoteIndex = emoteDuplicate.map((emote, index) => index);
-        let emoteInfo = emoteDuplicate.map((emote, index) => {
-            return (emote.animated) ? `${index}: <a:${emote.name}:${emote.id}> ${emote.guild.name}` : `${index}: <:${emote.name}:${emote.id}> ${emote.guild.name}`
-        })
-        emoteInfo = emoteInfo.join('\n');
-
-        const filter = (msg) => (msg.author.id === message.author.id && emoteIndex.includes(parseInt(msg.content)));
-
-        const reply = await message.reply(`${emoteDuplicate.length} emotes were found with the name ${emoteDuplicate[0].name}.\n`+
-            `Please select the number of the emote you want to use for the replacement. This command will automatically time out in 10 seconds.\n`+
-            `${emoteInfo}`);
-        const pickedEmote = await message.channel.awaitMessages(filter, {max: 1, maxProcessed: 2, time: 10000, errors: ['time']})
-                .then(collected => {
-                    if (collected.size === 0) {
-                        reply.edit(`Error received. Defaulting to first emote.`)
-                        let emote = emoteDuplicate[0];
-                        return (emote.animated) ? `<a:${emote.name}:${emote.id}>` : `<:${emote.name}:${emote.id}>`;
-                    } else {
-                        let emote = collected.map(collect => {
-                            let emotes = emoteDuplicate.filter((emote, index) => index === (parseInt(collect.content)));
-                            return emotes[0];
-                        });
-                        emote = emote[0];
-                        return (emote.animated) ? `<a:${emote.name}:${emote.id}>` : `<:${emote.name}:${emote.id}>` 
-                    }
-                })
-                .catch(() => {
-                    reply.edit(`Error received. Defaulting to first emote.`)
-                    let emote = emoteDuplicate[0];
-                    return (emote.animated) ? `<a:${emote.name}:${emote.id}>` : `<:${emote.name}:${emote.id}>`;
-                })
-        return pickedEmote;
-
-    }
+    
 });
 
 ///// LOG CLIENT INTO DISCORD
