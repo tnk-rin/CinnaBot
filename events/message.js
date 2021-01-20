930911e9035413df51190f4f1de44dac4e3620b1
Kazumik0108 <61595586+Kazumik0108@users.noreply.github.com>
2021-01-20 13:45:22 -0500

    added restrictions to emote command
    to prevent nitro abuse, message is not replaced for these additional conditions:
    - ANY escape character \ in the message
    - ANY backtick in the message
    - emote name is case-sensitive (may also aid with overlapping names, if the a user has any control over naming)
    
    A bypass currently exists, which can be resolved by restricting the emotes to servers the user is in (which may help with privacy if the bot is in a large number of servers)

--- events/message.js
+++ events/message.js
@@ -1,66 +1,7 @@
 // message.js
 
 ///// functions
-async function checkMessageEmotes(message) {
-    // Returns an array of emote substrings from the message that are eligible for replacement
-    /*  1) Match any message substrings in the form :name:, <:name:id> or <a:name:id>
-        2) If the substring starts with < and ends with >, then it is already in emote form
-        3) Else, it is eligible to replaced
-    */
-
-    const emoteRegex = /<a?:\w+:\d+>|(?<!\\):(\w+):/g;
-    const canReplace = message.content.match(emoteRegex).filter(substring => !(substring.startsWith('\<') && substring.endsWith('\>')));
-
-    return canReplace;
-}
-
-async function replaceMessageEmotes(message, canReplace) {
-    // Returns a string after attempting to replace the substrings of the original message
-
-    // get the emote caches from each guild
-    let emoteCache = [];
-    const guilds = await message.client.guilds.cache;
-    guilds.forEach(guild => {
-        emoteCache.push(guild.emojis.cache)
-    });
-
-    let content = message.content;
-    // attempt to match & replace a substring for each canReplace substring
-    canReplace.forEach(substring => {
-        content = content.replace(substring, (inputName) => {
-            // get the name from the input by removing colons
-            const name = inputName.replace(/\:/g,'');
-            
-            // get the array of matching emotes
-            let emoteMatched = []
-            for (let emotes of emoteCache) {
-                emotes.forEach(emote => {
-                    if (name.toLowerCase() === emote.name.toLowerCase()) emoteMatched.push(emote);
-                });
-            }
-            
-            // return the original substring if no matches were found
-            if (emoteMatched.length === 0) return substring;
-
-            // if multiple matches are found, prioritize using the emote in the messaged server
-            // otherwise, default to the first emote
-            if (emoteMatched.length >= 1) {
-                let emote = [];
-                if (emoteMatched.some(emote => emote.guild.id === message.guild.id)) {
-                    let emotes = emoteMatched.filter(emote => emote.guild.id === message.guild.id);
-                    emote = emotes[0];
-                } else {
-                    emote = emoteMatched[0];
-                }     
-                return (emote.animated) ? `<a:${emote.name}:${emote.id}>` : `<:${emote.name}:${emote.id}>`
-            };
-
-        });
-    });
-    return content;
-}
-
-async function sendMessageEmotes(message, content) {
+async function sendMessageWebhook(message, content) {
     // Deletes the OP's message and sends a webhook mimicking the OP
     message.delete();
     const webhooks = await message.channel.fetchWebhooks();
@@ -90,27 +31,48 @@ async function sendMessageEmotes(message, content) {
 }
 
 ///// exports
-module.exports = (client, message) => {
+module.exports = async (client, message) => {
     // return early if author is bot
     if (message.author.bot) return;
 
-    // emote replace block
-    /* Purpose: replace emotes in a message by a non-nitro user by checking the contents of a substring enclosed by a pair of colons, e.g. :emote:
-        An emote is eligible for replacement if it includes two colons,
-        as the array formed by splitting the message by colons will have a length greater than 2 if there are at least two colins in the message
-        However, if there are at least 2 backticks in the message, this command will abort (due to difficulty parsing out the necessary emotes inside/outside the code markup)
-    */
-    if (message.content.split(/\:/).length > 2 && message.content.split(/\`/).length <= 2) {
-        checkMessageEmotes(message)
-            .then(canReplace => {
-                // return if no eligible emotes were found
-                if (canReplace.length === 0) return;
-                return replaceMessageEmotes(message, canReplace)
-            .then(newMessage => {
-                // return if there were no changes
-                if (message.content === newMessage) return;
-                sendMessageEmotes(message, newMessage);
-                    })
-            })
-    } 
-}
+    // Emote replace block
+    // Attempt to replace emotes in a message by a non-nitro user by checking the contents of a substring enclosed by a pair of colons, e.g. :emote:
+    // To prevent nitro users from using this command, replacement will not be attempted if there is at least one escape character or backtick in the message
+    // Emote names are also case-sensitive, so users must type in the emote as written in the server!
+    if (message.content.split(/\:/).length > 2 && message.content.split(/\`/).length <= 1 && message.content.split(/\\/).length <= 1) {
+        const emoteRegex = /<a?:\w+:\d+>|(?<!\\):(\w+):/g;
+        let newMessage = message.content.replace(emoteRegex, replaceMessageEmotes);
+        // return if there were no changes to the message
+        if (message.content === newMessage) return;
+        await sendMessageWebhook(message, newMessage);
+    }
+
+
+
+
+
+
+    ///// functions
+    function replaceMessageEmotes(substring, match) {
+        // Returns a string after attempting to replace the emote substrings of the original message
+        if (match) {
+            let emoteMatch = getMatchEmojis(substring, match);
+            if (emoteMatch) {
+                return (emoteMatch.animated) ? `<a:${emoteMatch.name}:${emoteMatch.id}>` : `<:${emoteMatch.name}:${emoteMatch.id}>`;
+            }
+        }
+        // if no emote matches were found, return the original substring
+        return substring;
+    }
+    
+    function getMatchEmojis(substring, match) {
+        // name matches are case-sensitive to prevent nitro users from using this command
+        let name = emote => emote.name === match;
+        // prioritize the first emote found in the messaged server, otherwise get the first match in other servers
+        let emoteMatch = message.guild.emojis.cache.find(name);
+        if (!emoteMatch) {
+            emoteMatch = client.guilds.cache.flatMap(guild => guild.emojis.cache).find(name);
+        }
+        return emoteMatch;
+    }
+};
